603699768a3d563e3d4e0a29f7d749d509eba968
Shakeef Ahmed <shakeef.rakin321@gmail.com>
2024-01-07 12:14:12 +0600

    Changed style of the category buttons.

--- src/pages/Public/PublicHome/Categories/Categories.jsx
+++ src/pages/Public/PublicHome/Categories/Categories.jsx
@@ -1,10 +1,31 @@
 const Categories = () => {
   return (
     <div>
-      <div className="flex white gap-3 overflow-x-auto font-heading border-b p-5 text-lg">
-        <h1 className="font-bold text-center border-black border-b-2 hover:border-b-2">
+      <div className="flex white gap-3 overflow-x-auto font-heading border-b pb-5 text-lg">
+        <button className="btn bg-transparent px-8 text-white bg-primary hover:bg-primary border-transparent hover:border-transparent">
           All
-        </h1>
+        </button>
+        <button className="btn bg-transparent border-primary hover:border-primary hover:bg-transparent hover:shadow-xl px-8">
+          Technology
+        </button>
+        <button className="btn bg-transparent border-primary hover:border-primary hover:bg-transparent hover:shadow-xl px-8">
+          Food
+        </button>
+        <button className="btn bg-transparent border-primary hover:border-primary hover:bg-transparent hover:shadow-xl px-8">
+          Fashion
+        </button>
+        <button className="btn bg-transparent border-primary hover:border-primary hover:bg-transparent hover:shadow-xl px-8">
+          Travel
+        </button>
+        <button className="btn bg-transparent border-primary hover:border-primary hover:bg-transparent hover:shadow-xl px-8">
+          Health
+        </button>
+        <button className="btn bg-transparent border-primary hover:border-primary hover:bg-transparent hover:shadow-xl px-8">
+          Coding
+        </button>
+        <button className="btn bg-transparent border-primary hover:border-primary hover:bg-transparent hover:shadow-xl px-8">
+          DIY
+        </button>
       </div>
     </div>
   );
