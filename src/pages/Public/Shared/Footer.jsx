07aa6485a8db09c162156658c1ad925820f5ce7e
Shakeef Ahmed Rakin <shakeef.rakin321@gmail.com>
2024-01-19 01:13:10 +0600

    Testing.

--- src/pages/Public/Shared/Footer.jsx
+++ src/pages/Public/Shared/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <>
-      <h1 className="text-5xl text-red-500 font-bold">Footer</h1>
+      <h1 className="text-5xl text-red-500 font-bold">Footer changed</h1>
     </>
   );
 };
